b508f059eda645dba71d2829bdabd400616fc916
Jacek <bukowski.jacek@gmail.com>
2025-03-23 15:35:49 +0100

    correct translation testing

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -3,8 +3,22 @@
 namespace Tests;
 
 use Illuminate\Foundation\Testing\TestCase as BaseTestCase;
+use Mockery\MockInterface;
+use Modules\Invoices\Domain\Translation\TranslatorInterface;
 
 abstract class TestCase extends BaseTestCase
 {
-    // Removed RefreshDatabase to avoid requiring database for all tests
+    protected function setUp(): void
+    {
+        parent::setUp();
+
+        // Mock translator to always return keys for testing
+        $this->mock(TranslatorInterface::class, function (MockInterface $mock) {
+            $mock->shouldReceive('translate')
+                ->withAnyArgs()
+                ->andReturnUsing(function (string $key) {
+                    return $key;
+                });
+        });
+    }
 }
